4d914fa87957a9a8408e4dda6183ff57d38730b3
amanmulani09 <mulaniaman0504@gmail.com>
2026-06-15 10:20:36 +0530

    feat: creat load_docs

--- data-injection-pipeline/main.py
+++ data-injection-pipeline/main.py
@@ -7,11 +7,29 @@ from langchain_chroma import Chroma
 from dotenv import load_dotenv
 
 
+def load_docs(docs_path:str="./data") -> list[Document]:
+
+   if not os.path.exists(docs_path):
+      raise FileNotFoundError(f"Directory '{docs_path}' does not exist.")
+   
+   docs = []
+
+   for file in Path(docs_path).rglob("*.txt"):
+      text = file.read_text(encoding="utf-8")
+
+      docs.append(
+         Document(
+               page_content=text,
+               metadata={"source": str(file)}
+         )
+      )
+
+   return docs
 
 def main():
    
    # 1. loading the files 
-
+   docs = load_docs("./data")
     
 
 
