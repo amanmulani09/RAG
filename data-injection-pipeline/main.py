7b7033f677b1f567d9a94350030d718cf9936eac
amanmulani09 <mulaniaman0504@gmail.com>
2026-06-15 10:39:40 +0530

    feat: create chunking method

--- data-injection-pipeline/main.py
+++ data-injection-pipeline/main.py
@@ -7,7 +7,7 @@ from langchain_chroma import Chroma
 from dotenv import load_dotenv
 
 
-def load_docs(docs_path:str="./data") -> list[Document]:
+def load_docs(docs_path:str="./docs") -> list[Document]:
 
    if not os.path.exists(docs_path):
       raise FileNotFoundError(f"Directory '{docs_path}' does not exist.")
@@ -15,23 +15,51 @@ def load_docs(docs_path:str="./data") -> list[Document]:
    docs = []
 
    for file in Path(docs_path).rglob("*.txt"):
-      text = file.read_text(encoding="utf-8")
-
       docs.append(
          Document(
-               page_content=text,
-               metadata={"source": str(file)}
+               page_content=file.read_text(),
+               metadata={"source": str(file)},
          )
       )
 
-   return docs
+   return docs 
+
+def create_chunks(docs:list[Document], chunk_size:int=512, chunk_overlap:int=100) -> list[Document]:
+   
+   text_splitter = CharacterTextSplitter(
+      chunk_size=chunk_size,
+      chunk_overlap=chunk_overlap,
+   )
+
+   chunks = text_splitter.split_documents(docs)
+   
+   return chunks
+
+def create_embeddings_and_store(chunks:list[Document], persist_directory:str="db/chroma_db") -> Chroma:
+
+   embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+
+   # create chroma db vector store
+
+   vector_store = Chroma.from_documents(
+      documents=chunks,
+      embedding=embedding_model,
+      persist_directory=persist_directory
+   )
+   
 
 def main():
    
    # 1. loading the files 
-   docs = load_docs("./data")
-    
+   docs = load_docs("./docs")
+   # print(docs)
 
+   # 2. create chunks from the documents
+   chunks = create_chunks(docs)
+
+   # 3. create embeddings & store them in a vector database
+   vector_store = create_embeddings_and_store(chunks)
+   
 
 if __name__ == "__main__":
     main()
