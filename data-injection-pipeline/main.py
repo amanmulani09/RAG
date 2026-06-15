9be8b7a7ee9ddf7d256c832b1793616cc2600ea9
amanmulani09 <mulaniaman0504@gmail.com>
2026-06-15 10:10:57 +0530

    init : project setup with deps & docs

---
+++ data-injection-pipeline/main.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+from langchain_core.documents import Document
+from langchain_text_splitters import CharacterTextSplitter # for splitting the documents into chunks
+from langchain_openai import OpenAIEmbeddings  # to create embeddings for the documents
+from langchain_chroma import Chroma 
+from dotenv import load_dotenv
+
+
+
+def main():
+   
+   # 1. loading the files 
+
+    
+
+
+if __name__ == "__main__":
+    main()
